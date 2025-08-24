83d3016eb4c402c234429643c7da9f7ad4f19bbf
avisre <90497762+avisre@users.noreply.github.com>
2025-08-24 16:41:57 +0100

    Delete frontend directory

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// If you're on Tailwind v4 (postcss plugin moved), install @tailwindcss/postcss and swap to:
-// export default { plugins: { '@tailwindcss/postcss': {}, autoprefixer: {} } }
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
